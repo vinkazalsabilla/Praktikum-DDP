c8bbd2523a8eb2f207010f784ec37d02b4597032
vinkazalsabilla <114562947+vinkazalsabilla@users.noreply.github.com>
2022-12-20 06:08:16 -0800

    Add files via upload

---
+++ Tugas DDP/python/info_gempa.py
@@ -0,0 +1,31 @@
+from Gempa import Gempa
+
+# Membuat object gempa pertama
+gempa_1 = Gempa("Banten", 1.2)
+
+# Memanggil fungsi dampak() pada object gempa pertama
+gempa_1.dampak()
+
+# Membuat object gempa kedua
+gempa_2 = Gempa("Palu", 6.1)
+
+# Memanggil fungsi dampak() pada object gempa kedua
+gempa_2.dampak()
+
+# Membuat object gempa ketiga
+gempa_3 = Gempa("Cianjur", 5.6)
+
+# Memanggil fungsi dampak() pada object gempa ketiga
+gempa_3.dampak()
+
+# Membuat object gempa keempat
+gempa_4 = Gempa("Jayapura", 3.3)
+
+# Memanggil fungsi dampak() pada object gempa keempat
+gempa_4.dampak()
+
+# Membuat object gempa kelima
+gempa_5 = Gempa("Garut", 4.0)
+
+# Memanggil fungsi dampak() pada object gempa kelima
+gempa_5.dampak()
